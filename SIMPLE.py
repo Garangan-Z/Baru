ded94935e063293cbb23f3a02491f8eb3d6e5e20
☆ RAKA ☆™︻®╤──────═◍➤ <95731529+Garangan-Z@users.noreply.github.com>
2022-01-12 23:13:55 +0700

    Update SIMPLE.py

--- SIMPLE.py
+++ SIMPLE.py
@@ -73,9 +73,9 @@ raka_logo = '''
 \x1b[1;96m            \$$$$$$  |                            
 \x1b[1;96m             \______/                             
 \x1b[1;96m_______________________________________________________\n
-\x1b[1;96m⋆✥⋆➣\x1b[1;96mDesigen By : \x1b[1;97mRaka Andrian Tara
-\x1b[1;96m⋆✥⋆➣\x1b[1;96mGithub     : \x1b[1;97mBajingan-Z
-\x1b[1;96m⋆✥⋆➣\x1b[1;96mCoded By   : \x1b[1;97mRaka \x1b[1;92m& \033[0;37mAngga
+\x1b[1;92m Desigen By : \x1b[1;97mRaka Andrian Tara
+\x1b[1;92m Github     : \x1b[1;97mBajingan-Z
+\x1b[1;92m Coded By   : \x1b[1;97mRaka \x1b[1;92m& \033[0;37mAngga
 \x1b[1;96m_______________________________________________________
 '''
 raka_sayang_amanda = '3882176535153442'
