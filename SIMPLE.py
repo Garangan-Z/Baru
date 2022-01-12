0728436d3e49e7483d4137ff37b1f2eb918e93f7
☆ RAKA ☆™︻®╤──────═◍➤ <95731529+Garangan-Z@users.noreply.github.com>
2022-01-13 00:20:37 +0700

    Update SIMPLE.py

--- SIMPLE.py
+++ SIMPLE.py
@@ -228,7 +228,7 @@ def crack():
 		global loop, token
 		ra_pw = []
 		sys.stdout.write(
-		      '\r[%sR] Crack %s - %s \x1b[1;96mMohon Ditunggu... \x1b[1;97m! ' % (ra,loop, len(id))
+		      '\r[%sR] Crack %s - %s \x1b[1;96mMohon Ditunggu... \x1b[1;97m! ' % (bm,loop, len(id))
 		); sys.stdout.flush()
 		try:os.mkdir("results")
 		except OSError:pass
